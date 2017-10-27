05ebe806d2a5b909606b1746beba53305709681e
manoj <mailmanoj123@gmail.com>
2017-10-27 22:17:09 +0530

    initial

---
+++ helloDocker/src/main/java/hello/docker/controller/HomeController.java
@@ -0,0 +1,18 @@
+package hello.docker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Spring boot and Docker";
+	}
+	
+	@RequestMapping("/objective")
+	public String objective() {
+		return "To be a DevOps jombie !!!";
+	}
+}
---
+++ helloDocker/src/test/java/hello/docker/HelloDockerApplicationTests.java
@@ -0,0 +1,24 @@
+package hello.docker;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import hello.docker.controller.HomeController;
+
+public class HelloDockerApplicationTests {
+
+	@Test
+	public void testHome() {
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals(result, "Hello Spring boot and Docker");
+	}
+	
+	@Test
+	public void testObjective() {
+		HomeController hc = new HomeController();
+		String result = hc.objective();
+		assertEquals(result, "To be a DevOps jombie !!!");
+	}
+}
